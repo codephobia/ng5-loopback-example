9e9b93ca8c041966b378d28d3c45e697fc5289d6
Martin Raymond <codephobia@hotmail.com>
2018-04-13 23:20:58 -0700

    stream day 2 - flex layout

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule, BREAKPOINT } from '@angular/flex-layout';
 import { MatToolbarModule, MatMenuModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SDKBrowserModule } from './lbservices';
 import { UserNavComponent } from './user-nav/user-nav.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     declarations: [
@@ -17,6 +19,7 @@ import { HomeComponent } from './home/home.component';
         LoginComponent,
         UserNavComponent,
         HomeComponent,
+        FooterComponent,
     ],
     imports: [
         BrowserModule,
@@ -24,6 +27,7 @@ import { HomeComponent } from './home/home.component';
         SDKBrowserModule.forRoot(),
         FormsModule,
         BrowserAnimationsModule,
+        FlexLayoutModule,
         MatToolbarModule,
         MatMenuModule,
     ],
